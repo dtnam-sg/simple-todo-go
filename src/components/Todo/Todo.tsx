bf10e402dd1eea453a58719676a57e107821597d
namdt <namdt1@vnpay.vn>
2023-03-28 14:29:12 +0700

    refactor with useLocalStorage

--- src/components/Todo/Todo.tsx
+++ src/components/Todo/Todo.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState, useCallback } from "react";
+import useLocalStorage from "../../hooks/useLocalStorage";
 import { ITodo } from "../../model/todo.model";
 import TodoForm from "./TodoForm";
 import TodoList from "./TodoList";
@@ -6,22 +7,19 @@ import TodoList from "./TodoList";
 const TODO_KEY = "todos";
 
 const Todo = () => {
-  const [todos, setTodos] = useState<ITodo[]>([]);
+  const [todos, setTodos] = useLocalStorage<ITodo[]>(TODO_KEY, []);
 
-  useEffect(() => {
-    const storedTodos = JSON.parse(localStorage.getItem(TODO_KEY) || '[]');
-    setTodos(storedTodos);
-  }, []);
-
-  useEffect(() => {
-    localStorage.setItem(TODO_KEY, JSON.stringify(todos));
-  }, [todos]);
-
-
-  const addTodo = useCallback((text: string) => {
-    const newTodo: ITodo = { id: Date.now().toString(), text, completed: false };
-    setTodos((preTodos) => [...preTodos, newTodo]);
-  },[todos]);
+  const addTodo = useCallback(
+    (text: string) => {
+      const newTodo: ITodo = {
+        id: Date.now().toString(),
+        text,
+        completed: false,
+      };
+      setTodos((preTodos) => [...preTodos, newTodo]);
+    },
+    [todos],
+  );
 
   const handleToggleTodo = (id: string) => {
     const updatedTodos = todos.map((todo) => {
@@ -34,13 +32,16 @@ const Todo = () => {
     setTodos(updatedTodos);
   };
 
-  const handleDeleteTodo = useCallback((id: string) => {
-    setTodos(prevTodos => prevTodos.filter(todo => todo.id !== id));
-},[todos]);
+  const handleDeleteTodo = useCallback(
+    (id: string) => {
+      setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+    },
+    [todos],
+  );
 
   return (
-    <div className="container shadow-inner shadow-2xl mx-auto mt-8 max-w-xl bg-gradient-to-r from-cyan-300 bg-green-300 bg-green-200 p-5 rounded-xl text-center ">
-      <h1 className="text-3xl font-bold mb-4 text-green-600 ">Todo List</h1>
+    <div className='container shadow-inner shadow-2xl mx-auto mt-8 max-w-xl bg-gradient-to-r from-cyan-300 bg-green-300 bg-green-200 p-5 rounded-xl text-center '>
+      <h1 className='text-3xl font-bold mb-4 text-green-600 '>Todo List</h1>
       <TodoForm addTodo={addTodo} />
       <TodoList
         todos={todos}
