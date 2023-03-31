7fa31453395871c5cae444103a84d56e5a442a16
namdt <namdt1@vnpay.vn>
2023-03-31 17:07:17 +0700

    update kit button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,13 +1,61 @@
-import React, { ButtonHTMLAttributes } from 'react';
+import React, { FC, ButtonHTMLAttributes } from "react";
 
-type Props = ButtonHTMLAttributes<HTMLButtonElement>;
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: "primary" | "secondary" | "success" | "warning" | "danger" | undefined | '';
+  size?: "sm" | "md" | "lg";
+}
 
-const Button: React.FC<Props> = ({ children, ...props }) => {
+const Button: FC<ButtonProps> = ({
+  children,
+  variant = "primary",
+  size = "md",
+  className,
+  ...rest
+}) => {
+  const baseClasses =
+    "px-4 py-2 font-semibold rounded-lg focus:outline-none transition-all duration-200 ease-in-out";
+  let variantClasses = "";
+  let sizeClasses = "";
+
+  switch (variant) {
+    case "primary":
+      variantClasses = "text-white bg-blue-500 hover:bg-blue-600";
+      break;
+    case "secondary":
+      variantClasses =
+        "text-blue-500 bg-white border border-blue-500 hover:bg-blue-50";
+      break;
+    case "success":
+      variantClasses = "text-white bg-green-500 hover:bg-green-600";
+      break;
+    case "warning":
+      variantClasses = "text-white bg-yellow-500 hover:bg-yellow-600";
+      break;
+    case "danger":
+      variantClasses = "text-white bg-red-500 hover:bg-red-600";
+      break;
+    default:
+      variantClasses = "text-white";
+  }
+
+  switch (size) {
+    case "sm":
+      sizeClasses = "text-sm";
+      break;
+    case "md":
+      sizeClasses = "text-base";
+      break;
+    case "lg":
+      sizeClasses = "text-lg";
+      break;
+    default:
+      sizeClasses = "text-base";
+  }
+
   return (
     <button
-      {...props}
-      className="bg-blue-600 hover:bg-blue-700 px-3 py-2 text-white rounded-md focus:outline-none"
-    >
+      className={`${baseClasses} ${variantClasses} ${sizeClasses} ${className}`}
+      {...rest}>
       {children}
     </button>
   );
