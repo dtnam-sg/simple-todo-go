fd7fe3b4054b84a3f082d298be23ba16a9f6a79d
namdt <namdt1@vnpay.vn>
2023-03-31 14:02:19 +0700

    fix router base config

--- src/App.tsx
+++ src/App.tsx
@@ -8,15 +8,15 @@ const App = () => {
       <div className="pt-3">
         <ul className="flex justify-center items-center gap-4">
           <li className="nav-item">
-            <Link to="/" className="nav-link text-white hover:text-green-600">Home</Link>
+            <Link to="/todo-app-react" className="nav-link text-white hover:text-green-600">Home</Link>
           </li>
           <li className="nav-item">
-            <Link to="/alarm" className="nav-link text-white hover:text-green-600">Alarm</Link>
+            <Link to="todo-app-react/alarm" className="nav-link text-white hover:text-green-600">Alarm</Link>
           </li>
         </ul>
       </div>
       <Routes>
-        <Route path='/' element={<Todo />} />
+        <Route path='/todo-app-react' element={<Todo />} />
       </Routes>
     </>
   );
