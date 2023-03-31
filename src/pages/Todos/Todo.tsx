17fbe35e33624505305aff93d0a52901a5f63ebd
namdt <namdt1@vnpay.vn>
2023-03-31 13:58:15 +0700

    add simple pagination for todo list

--- src/pages/Todos/Todo.tsx
+++ src/pages/Todos/Todo.tsx
@@ -5,10 +5,11 @@ import TodoForm from "../../components/Todo/TodoForm";
 import TodoList from "../../components/Todo/TodoList";
 
 const TODO_KEY = "todos";
+const PAGE_SIZE = 5;
 
 const Todo = () => {
   const [todos, setTodos] = useLocalStorage<ITodo[]>(TODO_KEY, []);
-  const [showModalCreate, setShowModalCreate] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
 
   const addTodo = useCallback(
     (text: string) => {
@@ -40,15 +41,38 @@ const Todo = () => {
     [todos],
   );
 
+  const handlePageChange = (pageNumber: number) => {
+    setCurrentPage(pageNumber);
+  };
+
+  const totalPages = Math.ceil(todos.length / PAGE_SIZE);
+  const startIndex = (currentPage - 1) * PAGE_SIZE;
+  const endIndex = startIndex + PAGE_SIZE;
+
   return (
     <div className='font-sans text-xl container shadow-2xl mx-auto mt-8 max-w-xl bg-white p-5 rounded-xl text-center '>
       <h1 className='text-3xl font-bold mb-4 text-green-600 '>Todo List</h1>
       <TodoForm addTodo={addTodo} />
       <TodoList
-        todos={todos}
+        todos={todos.slice(startIndex, endIndex)}
         onToggleTodo={handleToggleTodo}
         onDeleteTodo={handleDeleteTodo}
       />
+      <div className='flex justify-center mt-6'>
+        {totalPages > 1 &&
+          Array.from({ length: totalPages }, (_, index) => (
+            <button
+              key={index}
+              className={`p-2 mx-2 ${
+                currentPage === index + 1
+                  ? "bg-blue-500 text-white"
+                  : "bg-white border border-gray-300 hover:bg-blue-500 hover:text-white text-gray-700"
+              }`}
+              onClick={() => handlePageChange(index + 1)}>
+              {index + 1}
+            </button>
+          ))}
+      </div>
     </div>
   );
 };
