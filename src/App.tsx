4bb3ea419856062bce7785d4b158d987de5fa310
namdt <namdt1@vnpay.vn>
2023-03-31 13:46:44 +0700

    change UI icon

--- src/App.tsx
+++ src/App.tsx
@@ -6,19 +6,18 @@ const App = () => {
   return (
     <>
       <div className="pt-3">
-        <ul className="flex justify-center items-center gap-4 underline">
+        <ul className="flex justify-center items-center gap-4">
           <li className="nav-item">
-            <Link to="/" className="nav-link">Home</Link>
+            <Link to="/" className="nav-link text-white hover:text-green-600">Home</Link>
           </li>
           <li className="nav-item">
-            <Link to="/alarm" className="nav-link">Alarm</Link>
+            <Link to="/alarm" className="nav-link text-white hover:text-green-600">Alarm</Link>
           </li>
         </ul>
       </div>
       <Routes>
         <Route path='/' element={<Todo />} />
       </Routes>
-      {/* <Todo /> */}
     </>
   );
 };
--- src/components/Todo/TodoItem.tsx
+++ src/components/Todo/TodoItem.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { TrashIcon } from '@heroicons/react/24/solid'
- 
+import { TrashIcon } from "@heroicons/react/24/solid";
+
 import { ITodoItemProps } from "../../model/todo.model";
 
 const TodoItem = ({ todo, onToggleTodo, onDeleteTodo }: ITodoItemProps) => {
@@ -12,21 +12,48 @@ const TodoItem = ({ todo, onToggleTodo, onDeleteTodo }: ITodoItemProps) => {
   };
 
   return (
-    <li className='flex items-center mb-2'>
-      <input
-        type='checkbox'
-        checked={todo.completed}
-        onChange={handleToggleTodo}
-        className='mr-2'
-      />
+    <li className='group flex items-center mb-2 '>
+      <div onClick={handleToggleTodo} className='mr-2'>
+        {!todo.completed ? (
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            stroke-width='1.5'
+            stroke='currentColor'
+            className='w-8 h-8'>
+            <path
+              stroke-linecap='round'
+              stroke-linejoin='round'
+              d='M21 12a9 9 0 11-18 0 9 9 0 0118 0z'
+            />
+          </svg>
+        ) : (
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            stroke-width='1.5'
+            stroke='currentColor'
+            className='w-8 h-8 text-green-500'>
+            <path
+              stroke-linecap='round'
+              stroke-linejoin='round'
+              d='M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z'
+            />
+          </svg>
+        )}
+      </div>
       <span
         className={`flex-1 ${
           todo.completed ? "line-through text-gray-500" : ""
         }`}>
         {todo.text}
       </span>
-      <button className='text-red-500 p-2 hover:border-red-400 border-red-700 hover:shadow-xl' onClick={handleDeleteTodo}>
-        <TrashIcon className="h-6 w-6 text-red-500 hover:text-red-600 border-red-700 " />
+      <button
+        className='text-red-500 p-2 hover:border-red-400 opacity-0 group-hover:opacity-100 '
+        onClick={handleDeleteTodo}>
+        <TrashIcon className='h-8 w-8 text-red-500 hover:text-red-600 border-red-700 ' />
       </button>
     </li>
   );
