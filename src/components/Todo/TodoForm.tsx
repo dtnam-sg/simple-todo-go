8ff1bb7aed20e230542bc44c85b64919a8906f62
namdt <namdt1@vnpay.vn>
2023-04-04 09:45:59 +0700

    update: props of event ts

--- src/components/Todo/TodoForm.tsx
+++ src/components/Todo/TodoForm.tsx
@@ -5,11 +5,11 @@ import Button from "../Button/Button";
 const TodoForm = ({ addTodo }: ITodoFormProps) => {
   const [text, setText] = useState("");
 
-  const handleChangeValue = (e) => {
+  const handleChangeValue = (e: React.ChangeEvent<HTMLInputElement>) => {
     setText(e.target.value);
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = (e: React.SyntheticEvent) => {
     e.preventDefault();
     if (!text || text.trim() === "") return;
     addTodo(text);
@@ -17,16 +17,16 @@ const TodoForm = ({ addTodo }: ITodoFormProps) => {
   };
 
   return (
-    <div className="flex mb-4 justify-center">
+    <div className='flex mb-4 justify-center'>
       <form onSubmit={handleSubmit}>
         <input
           type='text'
           value={text}
           onChange={handleChangeValue}
           placeholder='Add new todo'
-          className="flex-1 mr-2 py-2 h-10 px-4 border border-slate-200 rounded-lg hover:shadow focus:outline-none"
+          className='flex-1 mr-2 py-2 h-10 px-4 border border-slate-200 rounded-lg hover:shadow focus:outline-none'
         />
-        <Button type="submit">Add</Button>
+        <Button type='submit'>Add</Button>
       </form>
     </div>
   );
